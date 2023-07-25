7a13e9abaff754e22336c0b70d3ccfcc5d7edee2
TerryW624 <terrywade634@gmail.com>
2023-07-25 12:43:25 -0700

    set for cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
+describe('Filter, Add, and Delete Songs', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
+  })
+
+  it("Test #1: Filter through songs using search", () => {
+    
+  })
+
+  it("Test #2: Add new song using the form", () => {
+
+  })
+
+  it("Test #3: Delete Songs using the delete action button", () => {
+    
   })
 })
